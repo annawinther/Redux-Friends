b495fe9ef713ed982693164c4bf6353263441ea1
Anna W <anski.anna@gmail.com>
2019-06-20 15:18:59 +0100

    added some more props to the friends component

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Friend = props => {
-    return <li>{props.friends.name}</li>
+    return (
+        <div>
+            <h3>{props.friends.name}</h3>
+            <h3>{props.friends.age}</h3>
+            <h3>{props.friends.email}</h3>
+        </div>
+    )
 }
 
 export default Friend;
