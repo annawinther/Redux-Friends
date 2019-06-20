cf30a20d03af462147fdc866a97b3b1d716a6745
Anna W <anski.anna@gmail.com>
2019-06-20 17:36:06 +0100

    redirect working, log in working

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import Login from './components/Login';
-import './App.css';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import FriendListView from './views/FriendListView';
+
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <FriendListView />
-      <Login />
+        <BrowserRouter>
+          <Route 
+          exact path='/'
+          render={() => {
+            if (localStorage.getItem('token')){
+              return (
+              <FriendListView />
+              );
+            };
+            return <Redirect to="login" />
+          }} />
+
+          <Route path='/login' component={Login} />
+          {/* <Login /> */}
+         
+        </BrowserRouter>
     </div>
   );
 }
